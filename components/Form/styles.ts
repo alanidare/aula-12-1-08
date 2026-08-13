c9568ee0984bc16cbf1e38660c35e0019603dd05
Alani <alanilopeslopess@gmail.com>
2026-08-13 11:15:32 +0000

    add styles

---
+++ components/Form/styles.ts
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+
+ container: {
+    alignItems: 'center',
+    marginTop: 20,
+    justifyContent: 'center',
+    display: 'flex',
+    padding: 16,
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    width: '100%',
+ },
+
+input:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: '#160052',
+        borderRadius: 8,
+        padding: 12,
+        fontSize: 16,
+        marginBottom: 12,
+        backgroundColor: 'white',
+
+    },
+
+    button:{
+    backgroundColor: '#391266',
+    padding: 14,
+    borderRadius: 8,
+    alignItems: 'center',
+   justifyContent: 'center',
+    },
+
+    buttonText:{
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+    display: 'flex',
+    justifyContent: 'center',
+    },
+
+
+
+
+
+})
